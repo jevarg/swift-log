a8f9dcf3e647d52ba862e463cec875a6a315cf66
Jean Gravier <jean.gravier@hilo3d.com>
2020-07-28 16:41:47 +0200

    Fixed visibility issues

--- Sources/SwiftLog/Log.swift
+++ Sources/SwiftLog/Log.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-class Log {
-    enum MessageType: String {
+open class Log {
+    public enum MessageType: String {
         case debug = "🗻"
         case warning = "⚠️"
         case error = "🚫"
     }
 
-    fileprivate static func log(_ whatever: Any, type: MessageType, file: String, line: Int) {
+    private static func log(_ whatever: Any, type: MessageType, file: String, line: Int) {
         let filename: String
 
         if let substring = file.split(separator: "/").last {
@@ -27,17 +27,17 @@ class Log {
         print("\(type.rawValue) \(filename):\(line) -", whatever)
     }
 
-    static func debug(_ whatever: Any, file: String = #file, line: Int = #line) {
+    public static func debug(_ whatever: Any, file: String = #file, line: Int = #line) {
         #if DEBUG
         log(whatever, type: .debug, file: file, line: line)
         #endif
     }
 
-    static func warning(_ whatever: Any, file: String = #file, line: Int = #line) {
+    public static func warning(_ whatever: Any, file: String = #file, line: Int = #line) {
         log(whatever, type: .warning, file: file, line: line)
     }
 
-    static func error(_ whatever: Any, file: String = #file, line: Int = #line) {
+    public static func error(_ whatever: Any, file: String = #file, line: Int = #line) {
         log(whatever, type: .error, file: file, line: line)
     }
 }
